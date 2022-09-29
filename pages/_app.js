89fc74363bfcd8f07ba105020e66a75eb9ed7e23
Euris <teamtdm567@gmail.com>
2022-09-29 13:29:00 +0800

    rename site

--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,7 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>youtube-dl-web</title>
+        <title>ytdlp-web</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className="flex flex-col min-w-full min-h-screen bg-bunker-800">
